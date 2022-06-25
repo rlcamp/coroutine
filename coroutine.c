78b31e6039b82690aeda8afb5e314694202350ab
Richard Campbell <rlcamp@users.noreply.github.com>
2022-06-25 16:06:32 -0700

    Added cortex-m0plus fixme comment

--- coroutine.c
+++ coroutine.c
@@ -102,7 +102,7 @@ asm volatile( \
 : "+r"(_buf) : : "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31", "cc", "memory"); } while (0)
 
 #elif defined(__arm__)
-
+/* FIXME: "invalid immediate for address calculation (value = 0x00000012)" on cortex-m0plus */
 #define CONTEXT_BUF_SIZE_BYTES 12
 #define STACK_ALIGNMENT 16
 
