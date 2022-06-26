41b3d9464acef87ddb7f6835249d8a9672916cb5
Richard Campbell <rlcamp@users.noreply.github.com>
2022-06-25 21:06:31 -0700

    Fixed invalid offset on cortex-m

--- coroutine.c
+++ coroutine.c
@@ -102,7 +102,6 @@ asm volatile( \
 : "+r"(_buf) : : "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31", "cc", "memory"); } while (0)
 
 #elif defined(__arm__)
-/* FIXME: "invalid immediate for address calculation (value = 0x00000012)" on cortex-m0plus */
 #define CONTEXT_BUF_SIZE_BYTES 12
 #define STACK_ALIGNMENT 16
 
@@ -148,6 +147,7 @@ ADD_ONE_TO_R4_IF_THUMB \
 "mov fp, r3\n" \
 "mov sp, r1\n" \
 "bx r6\n" \
+".balign 4\n" \
 "0:\n" \
 : "+r"(_buf) : : "r1", "r2", "r3", "r4", "r5", "r6", "r8", "r9", "r10", NOT_THE_FRAME_POINTER, "r12", "lr", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "cc", "memory"); } while(0)
 
