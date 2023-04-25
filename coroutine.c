e76eaff2dd70a3e0122b466edc65fe9c228406ef
Richard Campbell <rlcamp@users.noreply.github.com>
2023-04-24 18:49:54 -0700

    added comments to aarch64 code

--- coroutine.c
+++ coroutine.c
@@ -100,30 +100,30 @@ asm volatile( \
 register void * _buf asm("x0") = buf; \
 register void * _func asm("x1") = func; \
 asm volatile( \
-"adr x4, 0f\n" \
-"mov x5, sp\n" \
-"stp x4, x5, [%0]\n" \
-"stp x29, x18, [%0, #16]\n" \
+"adr x4, 0f\n" /* compute address of end of this block of asm, which will be jumped to when returning to this context */ \
+"mov x5, sp\n" /* get the current stack pointer */ \
+"stp x4, x5, [%0]\n" /* store the old pc and sp */ \
+"stp x29, x18, [%0, #16]\n" /* store the old fp and r18 (which cannot be clobbered on apple, and harmless to special case it on others) */ \
 "mov x4, %1\n" /* load new pc */ \
 "mov x5, %0\n" /* load new sp */ \
-"mov sp, x5\n" \
-"br x4\n" \
+"mov sp, x5\n" /* switch to the new sp */ \
+"br x4\n" /* jump to the new pc */ \
 "0:\n" \
 : "+r"(_buf), "+r"(_func) : : "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31", "cc", "memory"); } while (0)
 
 #define SWAP_CONTEXT(buf) do { \
 register void * _buf asm("x0") = buf; \
 asm volatile( \
-"ldp x6, x7, [%0]\n" \
-"ldp x8, x9, [%0, #16]\n" \
-"adr x4, 0f\n" \
-"mov x5, sp\n" \
-"stp x4, x5, [%0]\n" \
-"stp x29, x18, [%0, #16]\n" \
-"mov x29, x8\n" \
-"mov x18, x9\n" \
-"mov sp, x7\n" \
-"br x6\n" \
+"ldp x6, x7, [%0]\n" /* load the saved pc and sp */ \
+"ldp x8, x9, [%0, #16]\n" /* load the saved fp and apple thing */ \
+"adr x4, 0f\n" /* compute address of end of this block of asm, which will be jumped to when returning to this context */ \
+"mov x5, sp\n" /* get the current stack pointer */ \
+"stp x4, x5, [%0]\n" /* store the old pc and sp */ \
+"stp x29, x18, [%0, #16]\n" /* store the old fp and apple thing */ \
+"mov x29, x8\n" /* move the new fp into place */ \
+"mov x18, x9\n" /* move the new apple thing into place */ \
+"mov sp, x7\n" /* switch to the new sp */ \
+"br x6\n" /* jump to the new pc */ \
 "0:\n" \
 : "+r"(_buf) : : "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31", "cc", "memory"); } while (0)
 
