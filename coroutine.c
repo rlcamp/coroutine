9bf425e787c0328d53c7a19b7718cdba0687c8d6
Richard Campbell <rlcamp@users.noreply.github.com>
2023-10-27 08:18:54 -0700

    revved year

--- coroutine.c
+++ coroutine.c
@@ -1,5 +1,5 @@
 /*
- Copyright 2015-2022 Richard Campbell
+ Copyright 2015-2023 Richard Campbell
  
  Permission to use, copy, modify, and/or distribute this software for any purpose with or without
  fee is hereby granted, provided that the above copyright notice and this permission notice appear
